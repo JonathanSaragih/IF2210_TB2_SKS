18caa265426a70bc3fb3100c59fa7972a377d232
satriadhikara <satriadhikara@gmail.com>
2024-05-21 04:00:56 +0700

    feat: init project

---
+++ src/main/java/satriadhikara/if2210_tb2_sks/HelloController.java
@@ -0,0 +1,14 @@
+package satriadhikara.if2210_tb2_sks;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
+public class HelloController {
+    @FXML
+    private Label welcomeText;
+
+    @FXML
+    protected void onHelloButtonClick() {
+        welcomeText.setText("Welcome to JavaFX Application!");
+    }
+}
