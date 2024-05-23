564bd7b17309060875f26034845327fc35e6b036
satriadhikara <satriadhikara@gmail.com>
2024-05-24 01:17:43 +0700

    refactor: update home

---
+++ src/main/java/jojikanabe/if2210_tb2_sks/HomeController.java
@@ -0,0 +1,44 @@
+package jojikanabe.if2210_tb2_sks;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import jojikanabe.if2210_tb2_sks.classes.GameState;
+
+import java.io.IOException;
+import java.util.EventListener;
+
+public class HomeController {
+    private Stage stage;
+    private Scene scene;
+    private Parent root;
+
+
+    public void NewGame(ActionEvent event) throws IOException {
+        GameState.getInstance().NewGame();
+        root = FXMLLoader.load(getClass().getResource("Player1.fxml"));
+        stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    public void LoadGame(ActionEvent event) throws IOException {
+        GameState.getInstance().LoadGame();
+        if (GameState.getInstance().giliran % 2 == 1) {
+            root = FXMLLoader.load(getClass().getResource("Player1.fxml"));
+        } else {
+            root = FXMLLoader.load(getClass().getResource("Player2.fxml"));
+        }
+        GameState.getInstance().LoadGame();
+        stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+    }
+
+
+}
