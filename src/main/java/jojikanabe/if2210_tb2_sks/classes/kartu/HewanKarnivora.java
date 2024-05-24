0b097b87cc657ad50f2aff9920d9e8d409bc791b
satriadhikara <satriadhikara@gmail.com>
2024-05-24 11:38:41 +0700

    refactor: fix and update makan

--- src/main/java/jojikanabe/if2210_tb2_sks/classes/kartu/HewanKarnivora.java
+++ src/main/java/jojikanabe/if2210_tb2_sks/classes/kartu/HewanKarnivora.java
@@ -1,12 +1,24 @@
 package jojikanabe.if2210_tb2_sks.classes.kartu;
 
 public class HewanKarnivora extends Hewan {
-    public HewanKarnivora(String nama,String image, Integer beratBadanSiapPanen, Integer beratBadan) {
-        super(nama,image, beratBadanSiapPanen, beratBadan);
+    public HewanKarnivora(String nama, String image, Integer beratBadanSiapPanen, Integer beratBadan) {
+        super(nama, image, beratBadanSiapPanen, beratBadan);
     }
 
     @Override
-    void makan(Integer beratBadan) {
-        this.beratBadan = beratBadan;
+    void makan(Kartu kartu) throws Exception {
+        String[] makanan = {"Sirip Hiu", "Daging Kuda", "Daging Domba", "Daging Beruang"};
+        if (kartu instanceof Produk) {
+            Produk produk = (Produk) kartu;
+            for (String s : makanan) {
+                if (produk.getNama().equals(s)) {
+                    this.beratBadan += produk.getKalori();
+                    return;
+                }
+            }
+            throw new Exception("Hewan tidak bisa makan produk ini");
+        } else {
+            throw new Exception("Hewan tidak bisa makan kartu ini");
+        }
     }
 }
