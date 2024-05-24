ffa75092ae792df3bb6afa6b92e5897f8e3c7018
fnathas <farrel.saskoro@gmail.com>
2024-05-25 06:51:49 +0700

    refactor: show card on shuffle menu

--- src/main/java/jojikanabe/if2210_tb2_sks/HomeController.java
+++ src/main/java/jojikanabe/if2210_tb2_sks/HomeController.java
@@ -1,6 +1,7 @@
 package jojikanabe.if2210_tb2_sks;
 
 import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.geometry.HPos;
 import javafx.geometry.Pos;
@@ -8,6 +9,7 @@ import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.*;
+import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.GridPane;
@@ -16,8 +18,13 @@ import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 import javafx.stage.StageStyle;
 import jojikanabe.if2210_tb2_sks.classes.GameState;
+import jojikanabe.if2210_tb2_sks.classes.Pemain;
+import jojikanabe.if2210_tb2_sks.classes.kartu.Kartu;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
 
 public class HomeController {
     private Stage stage;
@@ -25,6 +32,9 @@ public class HomeController {
     private Parent root;
 
     private TextField folderTextField;
+
+    @FXML
+    private Button deck0, deck1, deck2, deck3, deck4, deck5;
 
     public void NewGame(ActionEvent event) throws IOException {
         GameState.getInstance().NewGame();
@@ -152,11 +162,37 @@ public class HomeController {
         String text = folderTextField.getText();
     }
 
+    private Button createButtonShuffle(String nama, String imagePath) {
+        Button button = new Button(nama);
+        button.setPrefWidth(150);
+        button.setPrefHeight(250);
+        button.setStyle("-fx-border-radius: 20; -fx-background-radius: 20; -fx-border-width: 0;");
+
+        // Load the image
+        Image image = new Image(Objects.requireNonNull(getClass().getResourceAsStream(imagePath)));
+        ImageView imageView = new ImageView(image);
+        imageView.setFitWidth(50);
+        imageView.setFitHeight(50);
+
+        // Add the image to the button
+        button.setGraphic(imageView);
+        button.setContentDisplay(ContentDisplay.TOP);
+
+        return button;
+    }
+
+    private Button buttonShuffle1, buttonShuffle2, buttonShuffle3, buttonShuffle4;
+
+    private Kartu kartu1, kartu2, kartu3, kartu4;
+
     private void loadGameAndSwitchScene(ActionEvent event) throws IOException {
+        Pemain pemain;
         if (GameState.getInstance().giliran % 2 == 1) {
             root = FXMLLoader.load(getClass().getResource("Player1.fxml"));
+            pemain = GameState.getInstance().getPemain().get(0);
         } else {
             root = FXMLLoader.load(getClass().getResource("Player2.fxml"));
+            pemain = GameState.getInstance().getPemain().get(1);
         }
         stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
         scene = new Scene(root);
@@ -168,38 +204,66 @@ public class HomeController {
         alert.initStyle(StageStyle.UNDECORATED);
         alert.setHeaderText(null);
 
+        kartu1 = pemain.getDeck().getKartuRandom();
+        kartu2 = pemain.getDeck().getKartuRandom();
+        kartu3 = pemain.getDeck().getKartuRandom();
+        kartu4 = pemain.getDeck().getKartuRandom();
+
         GridPane gridPane = new GridPane();
         gridPane.setHgap(10);
         gridPane.setVgap(10);
         gridPane.setTranslateX(20);
         gridPane.setTranslateY(10);
 
-        Button button1 = new Button("");
-        button1.setPrefWidth(150);
-        button1.setPrefHeight(250);
+        buttonShuffle1 = createButtonShuffle(kartu1.getNama(), kartu1.getImage());
+        buttonShuffle2 = createButtonShuffle(kartu2.getNama(), kartu2.getImage());
+        buttonShuffle3 = createButtonShuffle(kartu3.getNama(), kartu3.getImage());
+        buttonShuffle4 = createButtonShuffle(kartu4.getNama(), kartu4.getImage());
 
-        Button button2 = new Button("");
-        button2.setPrefWidth(150);
-        button2.setPrefHeight(250);
-
-        Button button3 = new Button("");
-        button3.setPrefWidth(150);
-        button3.setPrefHeight(250);
-
-        Button button4 = new Button("");
-        button4.setPrefWidth(150);
-        button4.setPrefHeight(250);
-
-        gridPane.add(button1, 0, 0);
-        gridPane.add(button2, 1, 0);
-        gridPane.add(button3, 0, 1);
-        gridPane.add(button4, 1, 1);
+        gridPane.add(buttonShuffle1, 0, 0);
+        gridPane.add(buttonShuffle2, 1, 0);
+        gridPane.add(buttonShuffle3, 0, 1);
+        gridPane.add(buttonShuffle4, 1, 1);
 
         Button regenerateButton = new Button("Regenerate");
         regenerateButton.setPrefWidth(100);
         regenerateButton.setTranslateX(-5);
         regenerateButton.setTranslateY(10);
         regenerateButton.setId("regenerateButton");
+        regenerateButton.setOnAction(e -> {
+            pemain.getDeck().addKartu(kartu1);
+            pemain.getDeck().addKartu(kartu2);
+            pemain.getDeck().addKartu(kartu3);
+            pemain.getDeck().addKartu(kartu4);
+            kartu1 = pemain.getDeck().getKartuRandom();
+            kartu2 = pemain.getDeck().getKartuRandom();
+            kartu3 = pemain.getDeck().getKartuRandom();
+            kartu4 = pemain.getDeck().getKartuRandom();
+            buttonShuffle1.setText(kartu1.getNama());
+            buttonShuffle2.setText(kartu2.getNama());
+            buttonShuffle3.setText(kartu3.getNama());
+            buttonShuffle4.setText(kartu4.getNama());
+            Image image1 = new Image(Objects.requireNonNull(getClass().getResourceAsStream(kartu1.getImage())));
+            ImageView imageView1 = new ImageView(image1);
+            imageView1.setFitWidth(50);
+            imageView1.setFitHeight(50);
+            buttonShuffle1.setGraphic(imageView1);
+            Image image2 = new Image(Objects.requireNonNull(getClass().getResourceAsStream(kartu2.getImage())));
+            ImageView imageView2 = new ImageView(image2);
+            imageView2.setFitWidth(50);
+            imageView2.setFitHeight(50);
+            buttonShuffle2.setGraphic(imageView2);
+            Image image3 = new Image(Objects.requireNonNull(getClass().getResourceAsStream(kartu3.getImage())));
+            ImageView imageView3 = new ImageView(image3);
+            imageView3.setFitWidth(50);
+            imageView3.setFitHeight(50);
+            buttonShuffle3.setGraphic(imageView3);
+            Image image4 = new Image(Objects.requireNonNull(getClass().getResourceAsStream(kartu4.getImage())));
+            ImageView imageView4 = new ImageView(image4);
+            imageView4.setFitWidth(50);
+            imageView4.setFitHeight(50);
+            buttonShuffle4.setGraphic(imageView4);
+        });
 
         Button confirmButton = new Button("Confirm");
         confirmButton.setPrefWidth(100);
@@ -209,6 +273,10 @@ public class HomeController {
         confirmButton.setOnAction(e -> {
             alert.setResult(ButtonType.OK); // Set the result to OK (or any other type if needed)
             alert.hide(); // Close the alert dialog
+            pemain.getDeckAktif().add(kartu1);
+            pemain.getDeckAktif().add(kartu2);
+            pemain.getDeckAktif().add(kartu3);
+            pemain.getDeckAktif().add(kartu4);
         });
 
         VBox vbox = new VBox(10);
@@ -231,5 +299,51 @@ public class HomeController {
         alert.getButtonTypes().clear();
 
         alert.showAndWait();
+    }
+
+    public void addKartuToDeck() {
+        List<Kartu> deckAktif;
+        if (GameState.getInstance().giliran == 1) {
+            deckAktif = GameState.getInstance().getPemain().get(0).getDeckAktif();
+        } else {
+            deckAktif = GameState.getInstance().getPemain().get(1).getDeckAktif();
+        }
+        List<Button> deckButtons = new ArrayList<>();
+        deckButtons.add(deck0);
+        deckButtons.add(deck1);
+        deckButtons.add(deck2);
+        deckButtons.add(deck3);
+        deckButtons.add(deck4);
+        deckButtons.add(deck5);
+
+        for (Button button : deckButtons) {
+            button.setText("");
+            button.setGraphic(null);
+            button.setOnAction(null);
+        }
+
+        for (int i = 0; i < deckAktif.size(); i++) {
+            Button button = deckButtons.get(i);
+            Kartu kartu = deckAktif.get(i);
+            button.setText(kartu.getNama());
+
+            // Load the image
+            Image image = new Image(Objects.requireNonNull(getClass().getResourceAsStream(kartu.getImage())));
+            ImageView imageView = new ImageView(image);
+            imageView.setFitWidth(30); // Set the width of the image
+            imageView.setFitHeight(30); // Set the height of the image
+
+            // Add the image to the button
+            button.setGraphic(imageView);
+
+            button.setContentDisplay(ContentDisplay.TOP);
+        }
+
+    }
+
+    public void initialize() {
+        if (GameState.getInstance().giliran != null) {
+            addKartuToDeck();
+        }
     }
 }
--- src/main/java/jojikanabe/if2210_tb2_sks/classes/GameState.java
+++ src/main/java/jojikanabe/if2210_tb2_sks/classes/GameState.java
@@ -95,16 +95,16 @@ public class GameState implements ConfigController {
         dataKartu.add(new Produk("Telur", "/assets/kartu/Produk/telur.png", 50, 2));
         dataKartu.add(new Produk("Daging Beruang", "/assets/kartu/Produk/dagingberuang.png", 500, 12));
         dataKartu.add(new Produk("Jagung", "/assets/kartu/Produk/jagung.png", 150, 3));
-        dataKartu.add(new Produk("Labu", "/assets/kartu/Produk/pumpkin.png", 500, 10));
+        dataKartu.add(new Produk("Labu", "/assets/kartu/Produk/pumkin.png", 500, 10));
         dataKartu.add(new Produk("Stroberi", "/assets/kartu/Produk/strawberry.png", 350, 5));
 
         // Item
         dataKartu.add(new ItemAccelerate("/assets/kartu/item_Accelerate.png"));
-        dataKartu.add(new ItemDelay("/assets/kartu/delay.png"));
-        dataKartu.add(new ItemInstantHarvest("/assets/kartu/instant_harvest.png"));
-        dataKartu.add(new ItemDestroy("/assets/kartu/destroy.png"));
-        dataKartu.add(new ItemProtect("/assets/kartu/protect.png"));
-        dataKartu.add(new ItemTrap("/assets/kartu/trap.png"));
+        dataKartu.add(new ItemDelay("/assets/kartu/Item/delay.png"));
+        dataKartu.add(new ItemInstantHarvest("/assets/kartu/Item/instant_harvest.png"));
+        dataKartu.add(new ItemDestroy("/assets/kartu/Item/destroy.png"));
+        dataKartu.add(new ItemProtect("/assets/kartu/Item/protect.png"));
+        dataKartu.add(new ItemTrap("/assets/kartu/Item/trap.png"));
 
     }
 
